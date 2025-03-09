5410023f01123c93e528c2df5ea4761c80074d4a
dybbcnd <condechristiandave8@gmail.com>
2025-03-09 16:37:26 -0700

    update database.php

---
+++ database/database.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";    
+$dbname = "ipt_midterm_project";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+// echo "Connected successfully";
+?>
